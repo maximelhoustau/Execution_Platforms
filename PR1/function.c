d4fff5da497052261b66a992c0d9f7721b6b6a0b
Raphael REME <raphael.reme@orange.fr>
2018-12-14 08:51:00 +0100

    Add a code in C that should be equivalent to the program

---
+++ PR1/function.c
@@ -0,0 +1,12 @@
+int f(char *s) {
+    if(s == 0) return -1;
+    char c = *s;
+    if(c == 0) return 0;
+    
+    int r;
+    while (c != 0) {
+        r = (c != ' ');
+	      c = *(++s);
+    }
+    return r;
+}
